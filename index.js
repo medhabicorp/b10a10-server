018dad1e54daa40c6ba22c1a103ac8e00f05cf73
MdRezaulHoq <muradsust3@gmail.com>
2025-01-26 12:53:28 +0600

    created movie server

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+  res.send('Movie Portal Server is running')
+})
+
+app.listen(port, ()=>{
+  console.log(`Movie Portal Server is running on port: ${port}`)
+})
