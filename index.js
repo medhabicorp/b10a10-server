90ef6093fd14cd34bd60bce12c28f178c0f94ef8
MdRezaulHoq <muradsust3@gmail.com>
2025-01-28 11:39:47 +0600

    Add a movie feature completed

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gqz9f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
-// DB_USER=moviePortal
-// DB_PASS=7zDEArc8BdHRaEQ8
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -31,12 +28,33 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const movieCollection = client.db('movieDB').collection('movies'); 
+
+    app.get('/movies', async(req, res)=>{
+      const cursor = movieCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+    app.post("/movies", async (req, res) => {
+      const movie = req.body;
+      try {
+        const result = await client.db("movieDB").collection("movies").insertOne(movie);
+        res.send(result);
+      } catch (error) {
+        console.error(error);
+        res.status(500).send({ success: false, message: "Internal server error." });
+      }
+    });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
